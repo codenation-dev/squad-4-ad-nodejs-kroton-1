c8b77a4992abe10e7d14832282215a961d15ba03
Patrick Cruz <patrickcruz@MacBook-Pro-de-Patrick.local>
2020-03-21 16:17:05 -0300

    Tentativa 2

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const routes = require('./routes')
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+
+app.use(bodyParser.json())
+
+app.use('/v1', routes)
+app.use('/api-docs, swaggerUi.serve, swaggerUi.setup(swaggerDocument, options), )
+
+module.exports = { app }
