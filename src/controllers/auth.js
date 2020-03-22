e6ffac736c43c27f23cf003821a8bafaf11d2a75
pedrolf30 <pedrolf30@gmail.com>
2020-03-22 19:08:46 -0300

    Tirando comentario dos ifs

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,13 +4,13 @@ let Auth = {}
 
 Auth.getToken = (req, res, next) => {
 
-    // if(req.body.user === 'raul' && req.body.password === 'code123'){    
+    if(req.body.user === 'raul' && req.body.password === 'code123'){    
         const name  = req.body.user
         const token = jwt.sign({name}, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
         res.status(200).send({ token: token });
-    // }
+    }
     
-    // res.status(401).send({"error": "Invalid user or password."})
+    res.status(401).send({"error": "Invalid user or password."})
     
 }
 
