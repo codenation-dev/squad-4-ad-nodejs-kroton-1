870d8d1f3f64381795a7e133a84ba986bae8d135
Cristiano Franco <cristianofranco@Mac-Mini-Cristiano.local>
2020-03-22 20:15:59 -0300

    incluindo script de cryptografica [ch61]

---
+++ src/crypto/crty_dcryp.js
@@ -0,0 +1,23 @@
+const crypto = require("crypto");
+
+const criptRes ={ }
+
+const DADOS_CRIPTOGRAFAR = {
+    algoritmo : "aes256",
+    segredo : "chaves",
+    tipo : "hex"
+};
+
+criptRes.criptografar = (senha)=> {
+    const cipher = crypto.createCipher(DADOS_CRIPTOGRAFAR.algoritmo, DADOS_CRIPTOGRAFAR.segredo);
+    cipher.update(senha);
+    return cipher.final(DADOS_CRIPTOGRAFAR.tipo);
+}
+
+criptRes.descriptografar = (senha) => {
+    const decipher = crypto.createDecipher(DADOS_CRIPTOGRAFAR.algoritmo, DADOS_CRIPTOGRAFAR.segredo);
+    decipher.update(senha, DADOS_CRIPTOGRAFAR.tipo);
+    return decipher.final();
+}
+
+module.exports = criptRes
