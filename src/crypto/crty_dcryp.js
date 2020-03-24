4ba6815bd23539a3b55a1cff6eedfd3659074427
mbj-junior <mbj_junior@hotmail.com>
2020-03-23 21:15:29 -0300

    atualização do método de criptografia

--- src/crypto/crty_dcryp.js
+++ src/crypto/crty_dcryp.js
@@ -4,20 +4,22 @@ const criptRes ={ }
 
 const DADOS_CRIPTOGRAFAR = {
     algoritmo : "aes256",
-    segredo : "chaves",
-    tipo : "hex"
+    segredo : "chaveschaves1234chaveschaves1234",
+    tipo : "base64"
 };
 
 criptRes.criptografar = (senha)=> {
-    const cipher = crypto.createCipher(DADOS_CRIPTOGRAFAR.algoritmo, DADOS_CRIPTOGRAFAR.segredo);
-    cipher.update(senha);
-    return cipher.final(DADOS_CRIPTOGRAFAR.tipo);
+    const iv = crypto.randomBytes(16)
+    const cipher = crypto.createCipheriv(DADOS_CRIPTOGRAFAR.algoritmo, DADOS_CRIPTOGRAFAR.segredo, iv);
+    const crypted = cipher.update(senha, 'utf8', 'base64') + cipher.final('base64');
+    return iv.toString('base64')+':'+crypted;
 }
 
 criptRes.descriptografar = (senha) => {
-    const decipher = crypto.createDecipher(DADOS_CRIPTOGRAFAR.algoritmo, DADOS_CRIPTOGRAFAR.segredo);
-    decipher.update(senha, DADOS_CRIPTOGRAFAR.tipo);
-    return decipher.final();
+    const parts = senha.split(":")
+    const decipher = crypto.createDecipheriv(DADOS_CRIPTOGRAFAR.algoritmo, DADOS_CRIPTOGRAFAR.segredo, new Buffer.from(parts[0], 'base64'));
+    const plain = decipher.update(parts[1], 'base64', 'utf8')+decipher.final('utf8');
+    return plain;
 }
 
 module.exports = criptRes
