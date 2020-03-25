90dc67fa8f239994d70a9c1773b11bd99570869e
Patrick Cruz <patrickcruz@MacBook-Pro-de-Patrick.local>
2020-03-24 23:33:34 -0300

    documentação rodando sem erros

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ const app = express()
 const routes = require('./routes')
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-
-
+ 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/v1', routes)
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options))
 
 module.exports = { app }
